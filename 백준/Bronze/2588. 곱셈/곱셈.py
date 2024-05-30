2a9dcb483dd732ae036943fb6d68371d67cbf843
sebin.im <73998249+sebin0918@users.noreply.github.com>
2024-05-30 17:59:56 +0900

    other way to solve

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -5,3 +5,13 @@ for i in range(3, 0, -1):
     print(a*int(b[i-1]))
     
 print(a*int(b))
+
+
+
+a = int(input())
+b = input()
+
+print(a*int(b[2]))
+print(a*int(b[1]))
+print(a*int(b[0]))
+print(a*int(b))
