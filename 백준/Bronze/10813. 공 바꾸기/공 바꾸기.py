3c432334c4aead28835cd2813fda6681cd7cb37a
sebin.im <73998249+sebin0918@users.noreply.github.com>
2024-06-06 22:24:13 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+basket = [0]*N
+temp = 0
+
+for a in range(N):
+    basket[a] = a+1
+
+for a in range(M):
+    i, j = map(int, input().split())
+    temp = basket[i-1]
+    basket[i-1] = basket[j-1]
+    basket[j-1] = temp
+    
+for a in range(N):
+    print(basket[a], end = ' ')
+    
