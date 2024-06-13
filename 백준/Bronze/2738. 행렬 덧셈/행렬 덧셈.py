4bdc91b96670abce3583fcc0e21d0ec9a13a6eca
sebin.im <73998249+sebin0918@users.noreply.github.com>
2024-06-13 23:20:08 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+A, B = [],[]
+
+for i in range(N):
+    a =  list(map(int, input().split()))
+    A.append(a)
+for i in range(N):
+    b =  list(map(int, input().split()))
+    B.append(b)
+for i in range(N):
+    for j in range(M):
+        AB_sum = A[i][j] + B[i][j]
+        print(AB_sum, end=' ')
+    print()
