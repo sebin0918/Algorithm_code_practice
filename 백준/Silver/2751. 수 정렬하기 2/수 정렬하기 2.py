28f74c985f0354c654d690a40be077dcba184625
sebin.im <73998249+sebin0918@users.noreply.github.com>
2024-06-26 23:45:35 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1512 ms, Memory: 76972 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -0,0 +1,11 @@
+import sys
+
+N = int(input())
+num_list = []
+for i in range(N):
+    num = int(sys.stdin.readline())
+    num_list.append(num)
+    
+num_list.sort()
+for i in num_list:
+    print(i)
