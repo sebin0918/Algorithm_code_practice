d52430d9ad62c65b1029c0c3d970d13afb2949ba
sebin.im <73998249+sebin0918@users.noreply.github.com>
2024-07-01 01:45:17 +0900

    [Silver V] Title: 나이순 정렬, Time: 2900 ms, Memory: 46140 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = []
+
+for i in range(n):
+    age, name = map(str, input().split())
+    age = int(age)
+    arr.append((age, name))
+
+arr.sort(key = lambda x : x[0])
+
+for i in arr:
+    print(i[0], i[1])
